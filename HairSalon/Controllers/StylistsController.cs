d4735eed29a37d76b8690c2444650894a3f3ab3d
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-12 15:34:19 -0600

    add private database connectection; pass connection into constructor

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,9 +1,15 @@
+using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HairSalon.Controllers
 {
   public class StylistsController : Controller
   {
+    private readonly HairSalonContext _db;
 
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
   }
 }
