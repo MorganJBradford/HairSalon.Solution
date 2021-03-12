7678907e8a82ffbf632d9e220c5a0c885269b29b
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-12 11:46:50 -0600

    create database connection private field; pass field into constructor

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
   }
 }
