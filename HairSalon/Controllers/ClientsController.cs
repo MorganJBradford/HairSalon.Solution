b1d6d4052f44113cb8a68938ab824dc279c53336
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-12 11:56:39 -0600

    add Index route

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
@@ -9,6 +13,12 @@ namespace HairSalon.Controllers
     public ClientsController(HairSalonContext db)
     {
       _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+      return View(model);
     }
   }
 }
